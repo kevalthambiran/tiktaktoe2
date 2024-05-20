32eb8f0a67c716c6b60284ffc77f95fadde99659
keval <kevalthambiran2004@gmail.com>
2024-05-20 15:28:42 +0200

    first commit

---
+++ app/src/main/java/com/keval/tiktaktoe/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.keval.tiktaktoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+private lateinit var a1: Button
+private lateinit var a2: Button
+private lateinit var a3: Button
+
+private lateinit var b1: Button
+private lateinit var b2: Button
+private lateinit var b3: Button
+
+private lateinit var c1: Button
+private lateinit var c2: Button
+private lateinit var c3: Button
+
+private lateinit var tvTurn: TextView
+private lateinit var restart:ImageButton
+private lateinit var game: ConstraintLayout
+
+private var currentTurn: Int = 0
+private var play: Boolean = true
+
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        private lateinit var a1: Button
+        private lateinit var a2: Button
+        private lateinit var a3: Button
+
+        private lateinit var b1: Button
+        private lateinit var b2: Button
+        private lateinit var b3: Button
+
+        private lateinit var c1: Button
+        private lateinit var c2: Button
+        private lateinit var c3: Button
+
+        private lateinit var tvTurn: TextView
+        private lateinit var restart:ImageButton
+        private lateinit var game: ConstraintLayout
+
+        private var currentTurn: Int = 0
+        private var play: Boolean = true
+
+    }
+    }
+}
